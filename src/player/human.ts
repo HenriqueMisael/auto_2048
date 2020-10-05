8574ef970ed2f4d220554355eb9b08abd1800a42
Henrique Misael <henrique.nolear@gmail.com>
2020-10-05 11:20:29 -0300

    First deploy

--- src/player/human.ts
+++
@@ -1,8 +0,0 @@
-import { Player } from './player';
-
-export class KeyboardPlayer extends Player {
-  keyPressed(evt: { keyCode: number }) {
-    this.game.move(evt.keyCode);
-  }
-  turnPassed() {}
-}
--- src/player/random.ts
+++
@@ -1,9 +0,0 @@
-import { randomInt } from '../sketch';
-
-import { AutoPlayer } from './auto';
-
-export class RandomPlayer extends AutoPlayer {
-  protected get moveOption() {
-    return randomInt(0, 3);
-  }
-}
